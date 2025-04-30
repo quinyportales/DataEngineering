1165ea1303e7aaeb2924d22c08c2297aac732ce6
quinyportales <66879807+quinyportales@users.noreply.github.com>
2025-04-30 12:15:19 +0000

    CI error fixed

--- regular_patients_pyspark/regular_patients/main.py
+++ regular_patients_pyspark/regular_patients/main.py
@@ -3,13 +3,11 @@ from pathlib import Path
 from regular_patients import PatientsData
 
 if __name__ == '__main__':
-
     current_directory = Path(__file__).parent
-    INPUT_PATH = current_directory / 'input' / 'enroll.csv'
-    INPUT_PATH = f'file://{INPUT_PATH.as_posix()}'
-    END_DATE = '2016-09-30'
+    INPUT_PATH = str(current_directory / 'input' / 'enroll.csv')
+    OUTPUT_PATH = str(current_directory / 'output')
     START_DATE = '2015-10-01'
-    OUTPUT_PATH = str (current_directory / 'output')
+    END_DATE = '2016-09-30'
 
     patients = PatientsData(INPUT_PATH)
     #tranforming, filtering and writting the final result
